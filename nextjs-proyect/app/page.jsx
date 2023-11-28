f2fb34319cc2f77cc48c0bb4dcee377e1a4105ed
Natanael Sosa <51962394+Software26@users.noreply.github.com>
2023-11-28 07:22:35 -0400

    Update page.jsx

--- nextjs-proyect/app/page.jsx
+++ nextjs-proyect/app/page.jsx
@@ -1,7 +1,17 @@
-import React from "react";
 
-function IndexPage (){
+async function fetchUsers(){
+  const res = await fetch("https://reqres.in/api/users/2");
+  const data = await res.json();
+  return data.data
+}
+
+
+async function IndexPage(){
+  const users = await fetchUsers();
+  console.log(users)
+
   return(
+    
     <div>
       <h1>Index page</h1>
 
