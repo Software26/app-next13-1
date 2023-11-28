8af80b4c3b187613b9c900448a3e616dfdf654e3
Natanael Sosa <51962394+Software26@users.noreply.github.com>
2023-11-28 07:22:26 -0400

    Create page.jsx

---
+++ nextjs-proyect/app/services/page.jsx
@@ -0,0 +1,15 @@
+function ServicesPage(){
+    return(
+        <div>
+            <h1>Service</h1>
+            <p>Lorem ipsum dolor
+            sit amet consectetur adipisicing elit.
+             Placeat, alias reprehenderit molestias 
+             asperiores commodi impedit ex! Nulla 
+             neque, possimus sequi odit ratione 
+             excepturi quas est, nobis minima 
+             debitis quam aliquid.</p>
+        </div>
+    )
+}
+export default ServicesPage;
