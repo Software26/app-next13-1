c4a1f0b1371eb39504b51b0e419372b0349fd1c1
Natanael Sosa <51962394+Software26@users.noreply.github.com>
2023-11-28 07:22:19 -0400

    Update page.jsx

--- nextjs-proyect/app/about/page.jsx
+++ nextjs-proyect/app/about/page.jsx
@@ -2,7 +2,13 @@ function AboutPage(){
     return(
         <div>
             <h1>Hello About</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa ipsum blanditiis in temporibus quae 
+                perspiciatis quaerat expedita minus, harum e
+                sse possimus at aliquam tempora totam, suscipit, necessitatibus aperi
+                am voluptas rerum?
+            </p>
         </div>
     )
 }
-    export default AboutPage
+    export default AboutPage;
