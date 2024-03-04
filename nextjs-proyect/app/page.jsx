e1282a56b8eaf6de4f3d4a9dbed79b82cb0fee5c
Natanael Sosa <51962394+Software26@users.noreply.github.com>
2024-03-03 23:16:14 -0400

    Update page.jsx

--- nextjs-proyect/app/page.jsx
+++ nextjs-proyect/app/page.jsx
@@ -8,15 +8,26 @@ async function fetchUsers(){
 
 async function IndexPage(){
   const users = await fetchUsers();
-  console.log(users)
+  c
+
 
   return(
-    
-    <div>
+  <div>
       <h1>Index page</h1>
+        <ul>
+          {
+            users.map((user) => (
+              <li key={user.id}>
+                <div>
+                  <h5>{user.id} {user.first_name} {user.last_name}</h5>
+                  <p>{user.email}</p>
+                </div>
+                < img src={user.avatar} alt= {user.email} />
+              </li>
+            ))
+          }
+        </ul>
+      </div>
+  
 
-    </div>
-  )
-}
-
-export default IndexPage
+export default IndexPage;
